6412c81f66990f41e2a4f0f4b1786ca828573295
hieunugent <hieunugent@gmail.com>
2022-01-19 22:53:50 -0800

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,107 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
-function App() {
+
+const App = ()=> {
+  const [chosenLevel, setChosenLevel] = useState('2');
+  const [words, setWords] = useState(null);
+  const [correctAnswers, setCorrectAnswers] = useState([]);
+  const [clicked, setClicked] = useState([]);
+  const [score, setScore] = useState(0);
+  const getRandomWords= ()=> {
+
+     const options = {
+       method: "GET",
+       url: "https://twinword-word-association-quiz.p.rapidapi.com/type1/",
+       params: { level: chosenLevel, area: "sat" },
+       headers: {
+         "x-rapidapi-host": "twinword-word-association-quiz.p.rapidapi.com",
+         "x-rapidapi-key": process.env.REACT_APP_RAPID_API_KEY,
+       },
+     };
+
+     axios
+       .request(options)
+       .then((response) => {
+         console.log(response.data);
+         setWords(response.data);
+       })
+       .catch((error) => {
+         console.error(error);
+       });
+  }
+ console.log(words && words.quizlist)
+ 
+ useEffect(() => {
+  if(chosenLevel) getRandomWords();
+ }, [chosenLevel])
+  const checkAnswer = (option, optionIndex, correctAnswer)=> {
+  console.log(optionIndex, correctAnswer)
+  if (optionIndex == correctAnswer) {
+    setCorrectAnswers([...correctAnswers, option])
+    setScore(score => score + 1)
+  }else{
+    setScore(score => score - 1)
+  }
+  setClicked([...clicked, option])
+}
+
+  console.log('correctAnswer',correctAnswers)
+  console.log('clicked',clicked)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!chosenLevel && (
+        <div className="level-selector">
+          <h1>Word Association App</h1>
+          <p>Select Your level to start</p>
+          <select
+            name="levels"
+            id="levels"
+            value={chosenLevel}
+            onChange={(e) => setChosenLevel(e.target.value)}
+          >
+            <option value={null}>select level</option>
+            <option value="1">level 1</option>
+            <option value="2">level 2</option>
+            <option value="3">level 3</option>
+          </select>
+        </div>
+      )}
+      {chosenLevel && words && (
+        <div className="question-area">
+          <h1>Welcome to level: {chosenLevel}</h1>
+          <h3>Your Score is: {score}</h3>
+
+          <div className="questions">
+            {words.quizlist.map((question, _questionIndex) => (
+              <div key={_questionIndex} className="question-box">
+                {question.quiz.map((tip, _index) => (
+                  <p key={_index}>{tip}</p>
+                ))}
+
+                <div className="question-buttons">
+                  {question.option.map((option, optionIndex) => (
+                    <div className="question-button">
+                      <button
+                        disabled={clicked.includes(option)}
+                        onClick={() =>
+                          checkAnswer(option, optionIndex + 1, question.correct)
+                        }
+                      >
+                        {option}
+                      </button>
+
+                      {correctAnswers.includes(option) && <p>Correct!</p>}
+                    </div>
+                  ))}
+                </div>
+                <p>{question.correct}</p>
+              </div>
+            ))}
+          </div>
+          <button onClick={()=> setChosenLevel(null)}>Go Back</button>
+        </div>
+      )}
     </div>
   );
 }
